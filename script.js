bbeee9d6d3fa925b0bc119286e5baf5203f97e14
PSchnur <65939560+PSchnur@users.noreply.github.com>
2020-06-13 16:36:31 +0200

    Init pages

---
+++ script.js
@@ -0,0 +1,9 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+const message = "Thank you for submitting your request. The crew will respond shortly.";
+ 
+document.getElementById("formContact").addEventListener("submit", function(event){
+   event.preventDefault();
+   alert(message);
+});
